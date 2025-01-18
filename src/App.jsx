e4d39d078d26de6f243c98b3dac40e76a13f4b1a
Indrajith <indrajith.bodinayaka352@gmail.com>
2025-01-18 20:41:39 +0530

    add react router dom for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import HomePage from "./pages/HomePage"
+import { Outlet } from "react-router-dom"
+
 function App() {
 
   return (
-    <div>Hello world</div>
+    <>
+      <Outlet />
+    </>
   )
 }
 

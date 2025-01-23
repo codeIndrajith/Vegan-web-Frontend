b1ad782d708ad36e499370d7d363585deb8c6e0c
Indrajith <indrajith.bodinayaka352@gmail.com>
2025-01-23 23:38:34 +0530

    complete home page

--- src/components/HomeComponents/Navbar.jsx
+++ src/components/HomeComponents/Navbar.jsx
@@ -12,18 +12,20 @@ function Navbar() {
   return (
     <div className='flex items-center justify-between w-full h-[60px] px-4 bg-white'>
       {/* Logo */}
-      <div className='w-auto h-auto'>
+      <div className='w-auto h-auto flex gap-2'>
         <img src={logo} className='w-24 h-auto object-cover' alt="image logo" />
       </div>
 
       {/* Menu for larger screens */}
-      <div className='hidden md:flex items-center gap-8'>
+      <div className='hidden md:flex items-center gap-2 lg:gap-8'>
         <p className='text-sm md:text-md cursor-pointer'>Home</p>
         <p className='text-sm md:text-md cursor-pointer'>About</p>
         <p className='text-sm md:text-md cursor-pointer'>Explore</p>
         <p className='text-sm md:text-md cursor-pointer'>Shop</p>
         <p className='text-sm md:text-md cursor-pointer'>Community</p>
         <p className='text-sm md:text-md cursor-pointer'>My Delivery Details</p>
+        <button className='px-2 text-sm h-8 bg-black text-white rounded-md'>Sign In</button>
+        <button className='px-2 text-sm h-8 bg-black text-white rounded-md'>Sign Up</button>
       </div>
 
       {/* Toggle Button for small/medium screens */}
@@ -38,13 +40,17 @@ function Navbar() {
 
       {/* Menu for small/medium screens */}
       {menuOpen && (
-        <div className='absolute top-[60px] left-0 w-full bg-white shadow-lg z-10 flex flex-col h-screen gap-14 items-center px-4 py-2 md:hidden'>
+        <div className='absolute top-[60px] left-0 w-full bg-white shadow-lg z-10 h-auto gap-14 items-center px-4 py-8 md:hidden'>
           <p className='text-sm md:text-md cursor-pointer py-2'>Home</p>
           <p className='text-sm md:text-md cursor-pointer py-2'>About</p>
           <p className='text-sm md:text-md cursor-pointer py-2'>Explore</p>
           <p className='text-sm md:text-md cursor-pointer py-2'>Shop</p>
           <p className='text-sm md:text-md cursor-pointer py-2'>Community</p>
           <p className='text-sm md:text-md cursor-pointer py-2'>My Delivery Details</p>
+          <div className='flex gap-4'>
+          <button className='px-6 h-8 text-sm bg-black text-white rounded-md'>Sign In</button>
+          <button className='px-6 h-8 text-sm bg-black text-white rounded-md'>Sign Up</button>
+          </div>
         </div>
       )}
     </div>

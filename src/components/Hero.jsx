85575f686e56c3fdd8f3830f9c44c591eec14395
Indrajith <indrajith.bodinayaka352@gmail.com>
2025-01-23 20:08:04 +0530

    create home components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Navbar from './HomeComponents/Navbar'
+import LandinImage from './HomeComponents/LandinImage'
+import Product from './HomeComponents/Product'
+import Shop from './HomeComponents/Shop'
+import Food from './HomeComponents/Food'
+import Product2 from './HomeComponents/Product2'
+import Footer from './HomeComponents/Footer'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div>
+      <Navbar />
+      <LandinImage />
+      <Product />
+      <Shop />
+      <Food />
+      <Product2 />
+      <Footer />
+    </div>
   )
 }
 
---
+++ src/components/HomeComponents/ShopCarousel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ShopCarousel() {
+  return (
+    <div>ShopCarousel</div>
+  )
+}
+
+export default ShopCarousel
